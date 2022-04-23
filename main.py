65eb97a21468acb3d97765d698cff089157dea93
Baker Sharp <bakers@stanford.edu>
2022-04-22 19:46:27 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Any Card, Any Table.
+#
+# Play any collectible card game on any table using a projector!
+# A webcam looks down at a table surface, sees cardstock with barcodes on it,
+# and projects a specific card image onto that cardstock.
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
